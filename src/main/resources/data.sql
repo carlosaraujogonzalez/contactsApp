22f912b5e6f46c72f97fd7c4c75093f15b50e345
Carlos Araújo <carlosaraujogonzalez@gmail.com>
2020-01-20 23:26:16 +0100

    add database config

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into contact (id, firstName, lastname, phoneNumber, email)values(1, 'alex','raidentrance',+652672583, 'alex@gmail.com');
+insert into contact (id, firstName, lastname, phoneNumber, email)values(2, 'carlos','araujo gonzalez',+607716786, 'carlos@gmail.com');
+insert into contact (id, firstName, lastname, phoneNumber, email)values(3, 'juan','juanito dubalin',+654785211, 'juan@gmail.com');
